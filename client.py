86a55c5f7a5d9ee5dc69722cf7fa2f4e630e2cd9
Hamza HADJ AISSA <hadjaissahamza@gmail.com>
2023-11-15 00:05:48 +0100

    feat: implemented clients/server communication

--- client.py
+++ client.py
@@ -1,53 +1,72 @@
 import socket
 import threading
+import sys
+import select
+
 
 class Client:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.close_event = threading.Event()
 
     def connect(self):
-        self.client_socket.connect((self.host, self.port))
-        print(f"Connected to server on {self.host}:{self.port}")
-
-    def send_message(self):
-        try:
-            while True:
-                message = input(">> ")
-                if not message:
-                    pass
-                else:
-                    self.client_socket.send(message.encode('utf-8'))
-        except Exception as e:
-            print(f"Error sending message: {e}")
-        # finally:
-            # self.client_socket.close()
+        connection = self.server_socket.connect_ex((self.host, self.port))
+        if connection == 0:
+            print(f"Connected to server on {self.host}:{self.port}")
+        else:
+            print(f"Connection failed to server on {self.host}:{self.port}")
+            self.close_event.set()
+            sys.exit()
 
     def receive_messages(self):
-        try:
-            while True:
-                data = self.client_socket.recv(1024)
-                if not data:
-                    self.client_socket.close()
-                    break
-                message = data.decode('utf-8')
-                print(f"\n{message}", flush=True, end="")
-                
-        except Exception as e:
-            print(f"Error receiving message: {e}")
-        # finally:
-        #     self.client_socket.close()
+        while not self.close_event.is_set():
+            sockets_list = [sys.stdin, self.server_socket]
+            try:
+                read_sockets, write_socket, error_socket = select.select(
+                    sockets_list, [], [])
+                for socks in read_sockets:
+                    if socks == self.server_socket:
+                        message = socks.recv(2048)
+                        decoded_message = message.decode()
+                        if (decoded_message == "close"):
+                            print("Server is shutting down...!")
+                            self.server_socket.shutdown(socket.SHUT_RDWR)
+                            self.server_socket.close()
+                            self.close_event.set()
+                            # sys.exit()
+                        else:
+                            print(message.decode(), end="")
+            except socket.error as e:
+                print(f"Error connecting to the server: {e}")
+        print("Closing connection",
+              self.close_event.is_set())
+
+    def send_message(self):
+        while not self.close_event.is_set() and not sys.stdin.closed:
+            try:
+                message = sys.stdin.readline()
+                if len(message) > 0:
+                    self.server_socket.send(message.encode())
+            except Exception as e:
+                if {e} == "I/O operation on closed file":
+                    print({e})
+        print("Connection closed..!")
+
 
 if __name__ == "__main__":
-    client = Client('127.0.0.1', 12345)
+    # address = input("IP Address: ")
+    # port = input("Port: ")
+    client = Client("127.0.0.1", 12435)
     client.connect()
 
-    send_thread = threading.Thread(target=client.send_message)
+    send_thread = threading.Thread(target=client.send_message, daemon=True)
     receive_thread = threading.Thread(target=client.receive_messages)
 
     send_thread.start()
     receive_thread.start()
 
-    send_thread.join()
     receive_thread.join()
+    print("Exit...")
+    quit()
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket
 import sys
-from _thread import *
 import threading
 
 
@@ -10,64 +9,106 @@ class Server:
         self.port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.clients = []
+        self.close_event = threading.Event()
 
     def start(self):
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.setsockopt(
+            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(5)
-        print(f"Server listening on {self.host}:{self.port}\n")
+        print(f"Server listening on {self.host}:{self.port}")
 
-        while True:
-            client_socket, client_address = self.server_socket.accept()
-            print(f"Connection from {client_address}")
-            client_handler = threading.Thread(target=self.handle_client, args=(client_socket, client_address))
-            client_handler.start()
-            self.clients.append((client_socket, client_address))
+        while not self.close_event.is_set():
+            try:
+                client_socket, client_address = self.server_socket.accept()
+                print(
+                    f"Connection from client {client_address[0]}:{client_address[1]}")
 
-    def handle_client(self, client_socket, client_address):
+                # assign a thread for each new subscribed client
+                client_handler = threading.Thread(target=self._handle_client, args=(
+                    client_socket, client_address))
+                client_handler.start()
+                self.clients.append((client_socket, client_address))
+            except socket.error as e:
+                if 'bad file descriptor' in str(e):
+                    print("Server socket is closed or shutdown..!")
+                else:
+                    print(f"Error accepting or handling new connections: {e}")
+
+    # handle incoming messages for each client
+    def _handle_client(self, client_socket, client_address):
         try:
-            while True:
+            while not self.close_event.is_set():
                 data = client_socket.recv(1024)
                 if not data:
                     break
                 message = data.decode('utf-8')
-                print(f"{client_address[1]} << {message}")
-                self.broadcast(message, client_address)
+                print(client_address[1], "<<", message, end="")
+                self._broadcast(
+                    f"{client_address[1]} << {message}".encode('utf-8'),
+                    [client for client in self.clients if client[1] != client_address]
+                )
         except Exception as e:
+            # send a close signal to client's socket when ERROR
             print(f"Error handling client: {e}")
-        finally:
-            client_socket.close()
+            client_socket.send("close".encode('utf-8'))
 
-    def broadcast(self, message, client_address):
-        for client in self.clients:
+    # send a message to all clients
+    def send_message(self):
+        while not self.close_event.is_set():
+            if not sys.stdin.closed:
+                message = sys.stdin.readline()
+                if message.strip() == "close":
+                    self._close_server()
+                else:
+                    self._broadcast(
+                        f"server << {message}".encode(), self.clients)
+
+    def _broadcast(self, message, to):
+        for client in to:
             try:
-                if(client[1][1] != client_address[1]):
-                    client[0].send(f"{client[1][1]} << {message}".encode('utf-8'))
+                client[0].send(message)
             except Exception as e:
                 print(f"Error broadcasting to client: {e}")
                 # Remove the client from the list if there's an error
                 self.clients.remove(client)
-    
-    def close_server(self):
+
+    def _close_server(self):
+        print("Server is shutting down. Informing clients...")
+        # send closing message to all subscribed clients
+        for client_socket in self.clients:
+            try:
+                client_socket[0].send("close".encode('utf-8'))
+                print(
+                    f"Informeing client {client_socket[1][0]}:{client_socket[1][1]}...")
+            except Exception as e:
+                print(
+                    f"Error sending shutdown message to {client_socket}: {e}")
+
         try:
-            while True:
-                 message = input(">> ")
-                 if(message == "0"):
-                     print("Closing server...!")
-                     self.server_socket.shutdown(1)
-                     self.server_socket.close()
-                     return
+            # Close the server socket
+            self.server_socket.shutdown(socket.SHUT_RDWR)
+            self.server_socket.close()
+            self.close_event.set()
         except Exception as e:
-            print(f"Error closing server: {e}")
+            print({e})
+        sys.stdin.close()
+        print("Server has shut down.")
 
 
 if __name__ == "__main__":
-    server = Server('127.0.0.1', 12345)
+    # address = input("Host IP Address: ")
+    # port = input("Port: ")
+
+    server = Server("127.0.0.1", 12435)
     start_thread = threading.Thread(target=server.start)
-    close_thread = threading.Thread(target=server.close_server)
+    send_thread = threading.Thread(
+        target=server.send_message)
 
-    close_thread.start()
     start_thread.start()
+    send_thread.start()
 
-    start_thread.join()
-    close_thread.join()
+    # waiting for the closing flag
+    server.close_event.wait()
+    server.close_event.set()
+    print("Exiting...", server.close_event.is_set())
